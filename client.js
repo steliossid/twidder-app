2a76493eb2c24eed5c4ec88ff583863444ff5794
Stylianos Sidiropoulos <stsid95@gmail.com>
2020-01-24 14:16:14 +0100

    added lab1 files

---
+++ client.js
@@ -0,0 +1,14 @@
+displayView = function(){
+// the code required to display a view
+};
+
+window.onload = function(){
+//code that is executed as the page is loaded.
+//You shall put your own custom code here.
+​ //window.alert() is not allowed to be used in your implementation.
+window.alert("Hello TDDD97!");
+};
+
+//document.addEventListener('DOMContentLoaded', function() {
+//    alert("Prepei o html kwdikas na mpei se <script> kai na trexei otan anoigei....");
+//}, false);
