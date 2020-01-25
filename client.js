df1216a42fedf87b01cad5d1da62e0ae44a1f81b
Stylianos Sidiropoulos <stsid95@gmail.com>
2020-01-25 15:38:30 +0100

    'Display certain view' implemented but doesn't work

--- client.js
+++ client.js
@@ -1,10 +1,21 @@
-displayView = function(){
-// the code required to display a view
+displayView = function(welcome, profile){
+  // the code required to display a view
+  var display;
+  var isLoggedIn = FALSE;
+
+  if (!isLoggedIn) {
+    display = welcome;
+  }
+  else {
+    display = profile;
+  }
+  document.getElementById("content").innerHTML = display;
 };
 window.onload = function(){
-//code that is executed as the page is loaded.
-//You shall put your own custom code here.
-//window.alert() is not allowed to be used in your implementation.
-alert("Prepei o html kwdikas na mpei se <script> kai na trexei otan anoigei....");
-window.alert("Hello TDDD97!");
+  //code that is executed as the page is loaded.
+  //You shall put your own custom code here.
+  //window.alert() is not allowed to be used in your implementation.
+  var welcome = document.getElementById("welcomeview").innerHTML;
+  var profile = document.getElementById("profileview").innerHTML;
+  displayView(welcome, profile);
 };
