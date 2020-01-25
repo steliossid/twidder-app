72f1238dc1546b42bfa744bfad287519e2587024
Stylianos Sidiropoulos <stsid95@gmail.com>
2020-01-25 23:03:44 +0100

    password validation

--- client.js
+++ client.js
@@ -1,3 +1,10 @@
+window.onload = function(){
+  //code that is executed as the page is loaded.
+  var welcome = document.getElementById("welcomeview").innerHTML;
+  var profile = document.getElementById("profileview").innerHTML;
+  displayView(welcome, profile);
+};
+
 displayView = function(welcome, profile){
   // the code required to display a view
   var isLoggedIn = false;
@@ -10,11 +17,50 @@ displayView = function(welcome, profile){
   }
   document.getElementById("content").innerHTML = display;
 };
-window.onload = function(){
-  //code that is executed as the page is loaded.
-  //You shall put your own custom code here.
-  //window.alert() is not allowed to be used in your implementation.
-  var welcome = document.getElementById("welcomeview").innerHTML;
-  var profile = document.getElementById("profileview").innerHTML;
-  displayView(welcome, profile);
+
+var validSignInPassword = function(){
+  // Validation of Password at SignIn
+  // 1) Password is at least X characters long
+  var password = document.getElementById("signin_password").value;
+  var flag;
+  var message;
+
+  if(password.length>4){
+    message = "Password correct!";
+    flag = true;
+  }
+  else{
+    message = "Password needs to be at least 5 digits!";
+    flag = false;
+  }
+  document.getElementById("signin_password_message").innerHTML = message;
+  return flag;
+};
+
+var validSignupPassword = function(){
+  // Validation of Password at Signup
+  // 1) Both password fields must contain the same string
+  // 2) Password is at least X characters long
+  var password = document.getElementById("signup_password").value;
+  var rep_password = document.getElementById("signup_rep_password").value;
+  var flag;
+  var message;
+
+  if(password.length>4){
+    if(password==rep_password){
+      message = "Password correct!";
+      flag = true;
+    }
+    else{
+      message = "Passwords don't match!";
+      flag = false;
+    }
+  }
+  else{
+    message = "Password needs to be at least 5 digits!";
+    flag = false;
+  }
+
+  document.getElementById("signup_password_message").innerHTML = message;
+  return flag;
 };
