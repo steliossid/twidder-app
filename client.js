e22d269ba0fc907b8e920626b52b6444114648ea
VasileiosKarapoulios <billkarap123@gmail.com>
2020-01-26 14:43:47 +0100

    sign up completed

--- client.js
+++ client.js
@@ -22,42 +22,19 @@ displayView = function(isLoggedIn){
 };
 
 var check = function() {
-  if (document.getElementById('signup_password').value == document.getElementById('signup_rep_password').value) {
-    document.getElementById('signup_message').style.color = 'green';
-    document.getElementById('signup_message').innerHTML = 'Password matching';
-  } else {
-    document.getElementById('signup_message').style.color = 'red';
-    document.getElementById('signup_message').innerHTML = 'Password not matching';
-  }
+	if(document.getElementById('signup_password').value.length>4){
+	  if (document.getElementById('signup_password').value == document.getElementById('signup_rep_password').value) {
+		document.getElementById('signup_message').style.color = 'green';
+		document.getElementById('signup_message').innerHTML = 'Password matching';
+	  } else {
+		document.getElementById('signup_message').style.color = 'red';
+		document.getElementById('signup_message').innerHTML = 'Password not matching';
+	  }
+	}else{
+		document.getElementById('signup_message').style.color = 'red';
+		document.getElementById('signup_message').innerHTML = "Password needs to be at least 5 digits!";
+	}
 }
-
-var validSignupPassword = function(){
-  // Validation of Password at Signup
-  // 1) Both password fields must contain the same string
-  // 2) Password is at least X characters long
-  var password = document.getElementById("signup_password").value;
-  var rep_password = document.getElementById("signup_rep_password").value;
-  var flag;
-  var message;
-
-  if(password.length>4){
-    if(password==rep_password){
-      message = "Password correct!";
-      flag = true;
-    }
-    else{
-      message = "Passwords don't match!";
-      flag = false;
-    }
-  }
-  else{
-    message = "Password needs to be at least 5 digits!";
-    flag = false;
-  }
-
-  document.getElementById("signup_message").innerHTML = message;
-  return flag;
-};
 
 var validSignInPassword = function(){
   // Validation of Password at SignIn
@@ -67,11 +44,13 @@ var validSignInPassword = function(){
   var message;
 
   if(password.length>4){
-    message = "Password correct!";
+	document.getElementById('signin_password').style.color = 'green';
+    message = "Satisfies the length condition!";
     flag = true;
   }
   else{
-    message = "Password needs to be at least 5 digits!";
+	document.getElementById('signin_password').style.color = 'red';
+    message = "Needs to be at least 5 digits!";
     flag = false;
   }
   document.getElementById("signin_message").innerHTML = message;
@@ -79,37 +58,47 @@ var validSignInPassword = function(){
 };
 
 var signUpMechanism = function(){
-  var m = document.getElementById("selectmenu");
-  var user = {
-    email: document.getElementById('signup_username').value,
-    password: document.getElementById('signup_password').value,
-    firstname: document.getElementById('firstname').value,
-    familyname: document.getElementById('lastname').value,
-    gender: m.options[m.selectedIndex].value,
-    city: document.getElementById('city').value,
-    country: document.getElementById('country').value
-  };
-  var response = serverstub.signUp(user);
-
-  if(response['success']){
-    var frm = document.getElementById('formSignup');
-  	frm.reset();
-    // User must be redirected/logged in to his/her page
-  }
-  document.getElementById("signup_message").innerHTML = response['message'];
-  return response['success'];
+	if(document.getElementById('signup_message').innerHTML=='Password matching'){
+	  var m = document.getElementById("selectmenu");
+	  var user = {
+		email: document.getElementById('signup_username').value,
+		password: document.getElementById('signup_password').value,
+		firstname: document.getElementById('firstname').value,
+		familyname: document.getElementById('lastname').value,
+		gender: m.options[m.selectedIndex].value,
+		city: document.getElementById('city').value,
+		country: document.getElementById('country').value
+	  };
+	  var response = serverstub.signUp(user);
+	  if(response['message']=='User already exists.'){
+		document.getElementById("signup_username_message").style.color = 'red';
+		document.getElementById("signup_username_message").innerHTML = response['message'];
+	  }else{
+		document.getElementById("signup_username_message").style.color = 'green';
+		document.getElementById("signup_username_message").innerHTML = response['message'];
+	  }
+	  if(response['success']){
+		var frm = document.getElementById('formSignup');
+		frm.reset();
+		document.getElementById('signup_message').innerHTML = "";
+		// User must be redirected/logged in to his/her page
+	  }
+	}
+  
+  //document.getElementById("signup_username_message").innerHTML = response['message'];
+  //return response['success'];
 };
 
 var signInMechanism = function(){
-  email = document.getElementById('signin_username').value;
+	email = document.getElementById('signin_username').value;
 	password = document.getElementById('signin_password').value;
 	var response = serverstub.signIn(email, password);
 
-  if(response['success']){
-    displayView(isLoggedIn=true);
+	if(response['success']){
+		displayView(isLoggedIn=true);
 	}
-  document.getElementById("signin_message").innerHTML = response['message'];
-  return response['success'];
+	document.getElementById("signin_message").innerHTML = response['message'];
+	return response['success'];
 };
 
 var signOutMechanism = function(){
