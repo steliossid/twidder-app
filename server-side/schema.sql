929a76140dbc8e25a01b74d6d6136738c1fd17e6
VasileiosKarapoulios <billkarap123@gmail.com>
2020-02-05 20:28:49 +0100

    server-side

---
+++ server-side/schema.sql
@@ -0,0 +1,3 @@
+create table users(email varchar(30), password varchar(30), firstname varchar(30), familyname varchar(30), gender varchar(30), city varchar(30), country varchar(30), primary key(email));
+create table wall(email varchar(30), writer varchar(30), content varchar(30), primary key(email));
+create table loggedinusers(content varchar(30), token varchar(30), primary key(email));
