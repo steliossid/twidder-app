85b7d63bcf78926c630b01cf7b9a6c58a6d1080c
Stylianos Sidiropoulos <stsid95@gmail.com>
2020-02-11 22:33:33 +0100

    sign_in, change_password, get_user_messages_by_token, get_user_data_by_token

--- server-side/database_helper.py
+++ server-side/database_helper.py
@@ -3,11 +3,13 @@ from flask import g
 
 DATABASE_URI = 'database.db'
 
+
 def get_db():
     db = getattr(g, 'db', None)
     if db is None:
         db = g.db = sqlite3.connect(DATABASE_URI)
     return db
+
 
 def disconnect_db():
     db = getattr(g, 'db', None)
@@ -15,13 +17,16 @@ def disconnect_db():
         g.db.close()
         g.db = None
 
+
 def sign_up(email, password, firstname, familyname, gender, city, country):
     try:
-        get_db().execute("insert into users values(?,?,?,?,?,?,?);", [email, password, firstname, familyname, gender, city, country])
+        get_db().execute("insert into users values(?,?,?,?,?,?,?);",
+                         [email, password, firstname, familyname, gender, city, country])
         get_db().commit()
         return True
     except:
         return False
+
 
 def sign_out(token):
     try:
@@ -31,23 +36,26 @@ def sign_out(token):
     except:
         return False
 
-def get_user_data_by_email(email,token):
+
+def get_user_data_by_email(email, token):
     token_exists = get_db().execute('select count(*) from loggedinusers where token like ?', [token]).fetchall()
-    if token_exists[0][0]>0:
+    if token_exists[0][0] > 0:
         cursor = get_db().execute('select * from users where email like ?', [email])
         rows = cursor.fetchall()
         cursor.close()
         result = []
         for index in range(len(rows)):
-            result.append({'email': rows[index][0], 'password': rows[index][1], 'firstname': rows[index][2], 'familyname': rows[index][3], 'gender': rows[index][4],
-            'city': rows[index][5], 'country': rows[index][6]})
+            result.append({'email': rows[index][0], 'password': rows[index][1], 'firstname': rows[index][2],
+                           'familyname': rows[index][3], 'gender': rows[index][4],
+                           'city': rows[index][5], 'country': rows[index][6]})
         return result
     else:
         return False
 
-def get_user_messages_by_email(email,token):
+
+def get_user_messages_by_email(email, token):
     token_exists = get_db().execute('select count(*) from loggedinusers where token like ?', [token]).fetchall()
-    if token_exists[0][0]>0:
+    if token_exists[0][0] > 0:
         cursor = get_db().execute('select * from wall where email like ?', [email])
         rows = cursor.fetchall()
         cursor.close()
@@ -58,9 +66,10 @@ def get_user_messages_by_email(email,token):
     else:
         return False
 
-def post_message(email,content,token):
+
+def post_message(email, content, token):
     token_exists = get_db().execute('select count(*) from loggedinusers where token like ?', [token]).fetchall()
-    if token_exists[0][0]>0:
+    if token_exists[0][0] > 0:
         writer = get_db().execute('select email from loggedinusers where token like ?', [token]).fetchall()[0][0]
         try:
             get_db().execute("insert into wall values(?,?,?);", [email, writer, content])
@@ -68,3 +77,73 @@ def post_message(email,content,token):
             return True
         except:
             return False
+
+
+def find_user(email):
+   cursor = get_db().execute('select * from users where email like ?', [email])
+   rows = cursor.fetchall()
+   cursor.close()
+   result = []
+   for index in range(len(rows)):
+       result.append({'email': rows[index][0], 'password': rows[index][1],
+                      'firstname': rows[index][2], 'familyname': rows[index][3],
+                      'gender': rows[index][4], 'city': rows[index][5], 'country': rows[index][6]})
+   return result
+
+
+def sign_in(token, email):
+    try:
+        get_db().execute("insert into loggedinusers values(?,?);", [email, token])
+        get_db().commit()
+        return True
+    except:
+        return False
+
+
+def change_password(token, oldpassword, newpassword):
+    token_exists = get_db().execute('select count(*) from loggedinusers where token like ?', [token]).fetchall()
+    if token_exists[0][0] > 0:
+        user_email = get_db().execute('select email from loggedinusers where token like ?', [token]).fetchall()[0][0]
+        try:
+            get_db().execute('update users '
+                             'set password = ? '
+                             'where email in '
+                             '(select email '
+                             'from users '
+                             'where email like ? and password like ?);', [newpassword, user_email, oldpassword])
+            get_db().commit()
+            return True
+        except:
+            return False
+
+
+def get_user_messages_by_token(token):
+    token_exists = get_db().execute('select count(*) from loggedinusers where token like ?', [token]).fetchall()
+    if token_exists[0][0] > 0:
+        user_email = get_db().execute('select email from loggedinusers where token like ?', [token]).fetchall()[0][0]
+        cursor = get_db().execute('select * from wall where email like ?', [user_email])
+        rows = cursor.fetchall()
+        cursor.close()
+        result = []
+        for index in range(len(rows)):
+            result.append({'email': rows[index][0], 'writer': rows[index][1], 'conent': rows[index][2]})
+        return result
+    else:
+        return False
+
+
+def get_user_data_by_token(token):
+    token_exists = get_db().execute('select count(*) from loggedinusers where token like ?', [token]).fetchall()
+    if token_exists[0][0] > 0:
+        user_email = get_db().execute('select email from loggedinusers where token like ?', [token]).fetchall()[0][0]
+        cursor = get_db().execute('select * from users where email like ?', [user_email])
+        rows = cursor.fetchall()
+        cursor.close()
+        result = []
+        for index in range(len(rows)):
+            result.append({'email': rows[index][0], 'password': rows[index][1], 'firstname': rows[index][2],
+                           'familyname': rows[index][3], 'gender': rows[index][4],
+                           'city': rows[index][5], 'country': rows[index][6]})
+        return result
+    else:
+        return False
--- server-side/server.py
+++ server-side/server.py
@@ -1,74 +1,150 @@
 from flask import Flask, request, jsonify
 import database_helper
 import json
+import random
+import string
 
 app = Flask(__name__)
 
 app.debug = True
 
+
 @app.teardown_request
 def after_request(exception):
     database_helper.disconnect_db()
 
-@app.route('/users/sign_up', methods = ['PUT'])
+
+@app.route('/users/sign_up', methods=['PUT'])
 def sign_up():
     data = request.get_json()
     if 'email' in data and 'password' in data and 'firstname' in data and 'familyname' in data and 'gender' in data and 'city' in data and 'country' in data:
         if len(data['email']) <= 100 and len(data['password']) >= 5:
-            result = database_helper.sign_up(data['email'], data['password'], data['firstname'], data['familyname'], data['gender'], data['city'], data['country'])
-            if result == True:
-                return json.dumps({"success": "true", "message" : "User saved!"}), 200
+            result = database_helper.sign_up(data['email'], data['password'], data['firstname'], data['familyname'],
+                                             data['gender'], data['city'], data['country'])
+            if result:
+                return json.dumps({"success": "true", "message": "User saved!"}), 200
             else:
-                return json.dumps({"success": "false", "message" : "Something went wrong!"}), 500
+                return json.dumps({"success": "false", "message": "Something went wrong!"}), 500
         else:
             return '', 400
     else:
         return '', 400
 
-@app.route('/users/sign_out/<token>', methods = ['GET'])
-def sign_out(token = None):
+
+@app.route('/users/sign_out/<token>', methods=['GET'])
+def sign_out(token=None):
     if token is not None:
         result = database_helper.sign_out(token)
-        if result == True:
+        if result:
             return json.dumps({"success": "true", "message": "Token deleted!"}), 200
         else:
-            return json.dumps({"success": "false", "message" : "Something went wrong!"}), 500
+            return json.dumps({"success": "false", "message": "Something went wrong!"}), 500
     else:
         return '', 400
 
-@app.route('/users/get_user_data_by_email/<email>,<token>', methods = ['GET'])
-def get_user_data_by_email(email = None, token = None):
-    if email is not None and token is not None:
-        result = database_helper.get_user_data_by_email(email,token)
-        if result == False or result == None:
-            return json.dumps({"success": "false", "message" : "Something went wrong!"}), 500
-        else:
-            return json.dumps({"success": "true", "message" : "Requested user found!", "data": result}), 200
-            #return jsonify(result)
 
-@app.route('/users/get_user_messages_by_email/<email>,<token>', methods = ['GET'])
-def get_user_messages_by_email(email = None, token = None):
+@app.route('/users/get_user_data_by_email/<email>,<token>', methods=['GET'])
+def get_user_data_by_email(email=None, token=None):
     if email is not None and token is not None:
-        result = database_helper.get_user_messages_by_email(email,token)
+        result = database_helper.get_user_data_by_email(email, token)
         if result == False or result == None:
-            return json.dumps({"success": "true", "message" : "Something went wrong!"}), 500
+            return json.dumps({"success": "false", "message": "Something went wrong!"}), 500
         else:
-            return json.dumps({"success": "true", "message" : "Requested wall found!", "data": result}), 200
+            return json.dumps({"success": "true", "message": "Requested user found!", "data": result}), 200
+            # return jsonify(result)
 
-@app.route('/users/post_message', methods = ['PUT'])
+
+@app.route('/users/get_user_messages_by_email/<email>,<token>', methods=['GET'])
+def get_user_messages_by_email(email=None, token=None):
+    if email is not None and token is not None:
+        result = database_helper.get_user_messages_by_email(email, token)
+        if result == False or result == None:
+            return json.dumps({"success": "true", "message": "Something went wrong!"}), 500
+        else:
+            return json.dumps({"success": "true", "message": "Requested wall found!", "data": result}), 200
+
+
+@app.route('/users/post_message', methods=['PUT'])
 def post_message():
     data = request.get_json()
     if 'token' in data and 'content' in data and 'email' in data:
         if len(data['email']) <= 100:
             result = database_helper.post_message(data['email'], data['content'], data['token'])
-            if result == True:
-                return json.dumps({"success": "true", "message" : "Message posted to the wall!"}), 200
+            if result:
+                return json.dumps({"success": "true", "message": "Message posted to the wall!"}), 200
             else:
-                return json.dumps({"success": "false", "message" : "Something went wrong!"}), 500
+                return json.dumps({"success": "false", "message": "Something went wrong!"}), 500
         else:
             return '', 400
     else:
         return '', 400
+
+
+def token_gen(n):
+    letters = string.ascii_letters + "0123456789"
+    return ''.join(random.choice(letters) for i in range(n))
+
+
+@app.route('/users/get/<email>', methods=['GET'])
+def find_user(email=None):
+    if email is not None:
+        result = database_helper.find_user(email)
+        return jsonify(result)
+
+
+@app.route('/sign-in', methods=['POST'])
+def sign_in():
+    data = request.get_json()
+    if 'email' in data and 'password' in data:
+        if find_user('email')['email'] != None & find_user('email')['password'] == 'password':
+            token = token_gen(35)
+        else:
+            return '', 400
+        result = database_helper.sign_in(token, find_user('email')['email'])
+        if result:
+            return json.dumps({"success": True, "message": "Successfully signed in.", "data": token}), 200
+        else:
+            return json.dumps({"success": False, "message": "Something went wrong!"}), 500
+    else:
+        return '', 400
+
+
+@app.route('/change-password', methods=['PUT'])
+def change_password():
+    data = request.get_json()
+    if 'token' in data and 'oldpassword' in data and 'newpassword' in data:
+        if len(data['newpassword']) <= 30:
+            result = database_helper.change_password(data['token'], data['oldpassword'], data['newpassword'])
+            if result:
+                return json.dumps({"success": "true", "message": "Password changed!"}), 200
+            else:
+                return json.dumps({"success": "false", "message": "Something went wrong!"}), 500
+        else:
+            return '', 400
+    else:
+        return '', 400
+
+
+@app.route('/users/get_user_messages_by_token/<token>', methods=['GET'])
+def get_user_messages_by_token(token=None):
+    if token is not None:
+        result = database_helper.get_user_messages_by_token(token)
+        if result:
+            return json.dumps({"success": "true", "message": "Requested wall found!", "data": result}), 200
+        else:
+            return json.dumps({"success": "true", "message": "Something went wrong!"}), 500
+
+
+@app.route('/users/get_user_data_by_token/<token>', methods=['GET'])
+def get_user_data_by_token(token=None):
+    if token is not None:
+        result = database_helper.get_user_data_by_token(token)
+        if result:
+            return json.dumps({"success": "true", "message": "Requested user found!", "data": result}), 200
+
+        else:
+            return json.dumps({"success": "true", "message": "Something went wrong!"}), 500
+
 
 if __name__ == '__main__':
     app.run()
