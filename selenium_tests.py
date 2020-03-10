3e2598be7ab8901c2f7d4159c2d2bf050bd23e11
Stylianos Sidiropoulos <stsid95@gmail.com>
2020-03-10 17:28:30 +0200

    correct selenium_tests.py

--- selenium_tests.py
+++ selenium_tests.py
@@ -1,143 +1,163 @@
 # Module for Selenium tests
 
 import time
-import sys
 import server
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-if sys.argv[1] == "Chrome":
-    driver = webdriver.Chrome("drivers/chromedriver.exe")
-else:
-    driver = webdriver.Firefox("drivers/geckodriver.exe")
 
-# driver.get("http://127.0.0.1:5000/")
-driver.get("https://twidder-app-tddd97.herokuapp.com/")
+class TwidderTests(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome("drivers/chromedriver.exe")
 
-def run_tests():
-    if len(sys.argv[1:]) >= 1:
-        random_email = server.token_generator(10)
-        email = random_email + "@gmail.com"
+    def test_sign_up(self):
+        driver = self.driver
+        email = server.token_generator(10) + "@gmail.com"
         password = repeat_password = "password"
+        firstname = "Test"
+        familyname = "User"
+        gender = "Male"
+        city = "Linkoping"
+        country = "Sweden"
+        driver.get("https://twidder-app-tddd97.herokuapp.com/")
+
+        email_field = driver.find_element_by_name("signup_username")
+        password_field = driver.find_element_by_name("signup_password")
+        repeat_password_field = driver.find_element_by_name("rep_password")
+        firstname_field = driver.find_element_by_name("firstname")
+        familyname_field = driver.find_element_by_name("lastname")
+        gender_field = driver.find_element_by_name("gender")
+        city_field = driver.find_element_by_name("city")
+        country_field = driver.find_element_by_name("country")
+        signup_button = driver.find_element_by_name("signup_button")
+
+        email_field.send_keys(email)
+        password_field.send_keys(password)
+        repeat_password_field.send_keys(repeat_password)
+        firstname_field.send_keys(firstname)
+        familyname_field.send_keys(familyname)
+        gender_field.send_keys(gender)
+        city_field.send_keys(city)
+        country_field.send_keys(country)
+
+        signup_button.send_keys(Keys.RETURN)
+        time.sleep(2)
+        signup_message = driver.find_element_by_id("signup_username_message").text
+        # print(signup_message)
+        assert signup_message == "User saved!"
+
+    def test_sign_in(self):
+        driver = self.driver
+        email = "test@gmail.com"
+        password = "password"
+        driver.get("https://twidder-app-tddd97.herokuapp.com/")
+
+        email_field = driver.find_element_by_name("signin_username")
+        password_field = driver.find_element_by_name("signin_password")
+        login_button = driver.find_element_by_name("login_button")
+
+        email_field.send_keys(email)
+        password_field.send_keys(password)
+
+        login_button.send_keys(Keys.RETURN)
+        time.sleep(2)
+        signin_message = driver.find_element_by_id("right_email").text
+        # print(signin_message)
+
+        assert signin_message == email
+
+    def test_post_message(self):
+        driver = self.driver
+        email = "test@gmail.com"
+        password = "password"
+        driver.get("https://twidder-app-tddd97.herokuapp.com/")
+        email_field = driver.find_element_by_name("signin_username")
+        password_field = driver.find_element_by_name("signin_password")
+        login_button = driver.find_element_by_name("login_button")
+        email_field.send_keys(email)
+        password_field.send_keys(password)
+        login_button.send_keys(Keys.RETURN)
+        time.sleep(2)
+
         message1 = "This is an automated message from Selenium (Home Tab)"
-        message2 = "This is an automated message from Selenium (Browse Tab)"
-        test_sign_up(email, password, repeat_password, "Test", "User", "Male", "Linkoping", "Sweden")
-        test_sign_in(email, password)
-        test_post_message(message1)
+
+        post_message_filed = driver.find_element_by_name("post_text")
+        post_button = driver.find_element_by_name("post_button")
+
+        post_message_filed.send_keys(message1)
+        post_button.send_keys(Keys.RETURN)
+        time.sleep(2)
+        post_message = driver.find_element_by_id("post_message").text
+        # print(post_message)
+
+        assert post_message == "Message posted to the wall!"
+
+    def test_search_user(self):
+        driver = self.driver
+        email = "test@gmail.com"
+        password = "password"
+        driver.get("https://twidder-app-tddd97.herokuapp.com/")
+        email_field = driver.find_element_by_name("signin_username")
+        password_field = driver.find_element_by_name("signin_password")
+        login_button = driver.find_element_by_name("login_button")
+        email_field.send_keys(email)
+        password_field.send_keys(password)
+        login_button.send_keys(Keys.RETURN)
+        time.sleep(2)
+
         driver.find_element_by_name("browse").send_keys(Keys.RETURN)
-        test_search_user(email)
-        test_post_message_to_others(message2)
-        driver.find_element_by_name("account").send_keys(Keys.RETURN)
-        new_password = "newpassword"
-        test_change_password(oldpassword=password, newpassword=new_password, repeat_newpassword=new_password)
-        test_logout()
-        driver.close()
-    else:
-        print("Please specify the browser you would like to run the tests! (Chrome or Firefox)")
-        sys.exit(1)
+        email_filed = driver.find_element_by_name("search_email")
+        search_button = driver.find_element_by_name("button_search")
 
+        email_filed.send_keys(email)
 
-def test_sign_in(email, password):
-    email_field = driver.find_element_by_name("signin_username")
-    password_field = driver.find_element_by_name("signin_password")
-    login_button = driver.find_element_by_name("login_button")
+        search_button.send_keys(Keys.RETURN)
+        time.sleep(1)
+        search_message = driver.find_element_by_id("user_search_message").text
+        # print(search_message)
 
-    email_field.send_keys(email)
-    time.sleep(1)
-    password_field.send_keys(password)
-    time.sleep(1)
+        assert search_message == "Requested user found!"
 
-    login_button.send_keys(Keys.RETURN)
-    time.sleep(1)
+    def test_post_message_to_others(self):
+        driver = self.driver
+        email = "test@gmail.com"
+        password = "password"
+        driver.get("https://twidder-app-tddd97.herokuapp.com/")
+        email_field = driver.find_element_by_name("signin_username")
+        password_field = driver.find_element_by_name("signin_password")
+        login_button = driver.find_element_by_name("login_button")
+        email_field.send_keys(email)
+        password_field.send_keys(password)
+        login_button.send_keys(Keys.RETURN)
+        time.sleep(2)
 
+        message2 = "This is an automated message from Selenium (Browse Tab)"
 
-def test_sign_up(email, password, repeat_password, firstname, familyname, gender, city, country):
-    email_field = driver.find_element_by_name("signup_username")
-    password_field = driver.find_element_by_name("signup_password")
-    repeat_password_field = driver.find_element_by_name("rep_password")
-    firstname_field = driver.find_element_by_name("firstname")
-    familyname_field = driver.find_element_by_name("lastname")
-    gender_field = driver.find_element_by_name("gender")
-    city_field = driver.find_element_by_name("city")
-    country_field = driver.find_element_by_name("country")
-    signup_button = driver.find_element_by_name("signup_button")
+        driver.find_element_by_name("browse").send_keys(Keys.RETURN)
+        email_filed = driver.find_element_by_name("search_email")
+        search_button = driver.find_element_by_name("button_search")
 
-    email_field.send_keys(email)
-    time.sleep(1)
-    password_field.send_keys(password)
-    time.sleep(1)
-    repeat_password_field.send_keys(repeat_password)
-    time.sleep(1)
-    firstname_field.send_keys(firstname)
-    time.sleep(1)
-    familyname_field.send_keys(familyname)
-    time.sleep(1)
-    gender_field.send_keys(gender)
-    time.sleep(1)
-    city_field.send_keys(city)
-    time.sleep(1)
-    country_field.send_keys(country)
-    time.sleep(1)
+        email_filed.send_keys(email)
 
-    signup_button.send_keys(Keys.RETURN)
-    time.sleep(1)
+        search_button.send_keys(Keys.RETURN)
+        time.sleep(1)
 
+        post_message_filed = driver.find_element_by_name("post_text_others")
+        post_button = driver.find_element_by_name("post_button_others")
 
-def test_post_message(message):
-    post_message_filed = driver.find_element_by_name("post_text")
-    post_button = driver.find_element_by_name("post_button")
+        post_message_filed.send_keys(message2)
 
-    post_message_filed.send_keys(message)
-    time.sleep(1)
+        post_button.send_keys(Keys.RETURN)
+        time.sleep(1)
+        post_others_message = driver.find_element_by_id("post_message_others").text
+        # print(post_others_message)
 
-    post_button.send_keys(Keys.RETURN)
-    time.sleep(1)
+        assert post_others_message == "Message posted to the wall!"
 
-
-def test_search_user(email):
-    email_filed = driver.find_element_by_name("search_email")
-    search_button = driver.find_element_by_name("button_search")
-
-    email_filed.send_keys(email)
-    time.sleep(1)
-
-    search_button.send_keys(Keys.RETURN)
-    time.sleep(1)
-
-
-def test_post_message_to_others(message):
-    post_message_filed = driver.find_element_by_name("post_text_others")
-    post_button = driver.find_element_by_name("post_button_others")
-
-    post_message_filed.send_keys(message)
-    time.sleep(1)
-
-    post_button.send_keys(Keys.RETURN)
-    time.sleep(1)
-
-
-def test_change_password(oldpassword, newpassword, repeat_newpassword):
-    oldpassword_filed = driver.find_element_by_name("old_password")
-    newpassword_field = driver.find_element_by_name("new_password")
-    repeat_newpassword_field = driver.find_element_by_name("rep_new_password")
-    change_password_button = driver.find_element_by_name("button_change")
-
-    oldpassword_filed.send_keys(oldpassword)
-    time.sleep(1)
-    newpassword_field.send_keys(newpassword)
-    time.sleep(1)
-    repeat_newpassword_field.send_keys(repeat_newpassword)
-    time.sleep(1)
-
-    change_password_button.send_keys(Keys.RETURN)
-    time.sleep(1)
-
-
-def test_logout():
-    logout_button = driver.find_element_by_id("buttonSignOut")
-    logout_button.send_keys(Keys.RETURN)
-    time.sleep(3)
+    def tearDown(self):
+        self.driver.close()
 
 
 if __name__ == '__main__':
-    run_tests()
+    unittest.main()
